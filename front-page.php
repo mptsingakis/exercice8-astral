c43dfe944744b7446e65220d5471a10690418dfa
Tsingakis MP <marie-pier.1997@hotmail.com>
2020-02-28 15:58:31 -0500

    ex8:astral ->news

---
+++ front-page.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * The template for displaying all page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Astral
+ * @since 0.1
+ */
+get_header();
+/* 
+* Functions hooked into astral_top_banner action
+* 
+* @hooked astral_inner_banner
+*/
+do_action( 'astral_top_banner' );
+/* 
+* Functions hooked into astral_breadcrumb_area action
+* 
+* @hooked astral_breadcrumb_area
+*/
+do_action( 'astral_breadcrumb_area' );
+?>
+<div id="content">
+    <section class="align-blog" id="blog">
+        <div class="container">
+            <div class="row">
+                <!-- left side -->
+                <div class="col-lg-8 single-left mt-lg-0 mt-4">
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+						get_template_part( 'post', 'page' );
+					endwhile;
+                    endif;
+                    
+                    // The Query
+
+$args = array(
+    "category_name" => "nouvelle",
+    "posts_per_page" => 3, //afficher les 3 derniere nouvelles "posts_per_page" => "3"
+   // "orderby" =>"date",
+   // "order" => "ASC"
+);
+
+$query1 = new WP_Query( $args );
+ 
+// The Loop
+while ( $query1->have_posts() ) {
+    $query1->the_post();
+    echo '<h2>' . get_the_title() . '</h2>';
+    echo '<p>' . SUBSTR(get_the_excerpt(),0,300) . '</p>';
+}
+ 
+/* Restore original Post Data 
+ * NB: Because we are using new WP_Query we aren't stomping on the 
+ * original $wp_query and it does not need to be reset with 
+ * wp_reset_query(). We just need to set the post data back up with
+ * wp_reset_postdata().
+ */
+wp_reset_postdata();
+ 
+ 
+/* The 2nd Query (without global var) */
+
+$args2 = array(
+    "category_name" => "evenements",
+    "posts_per_page"=> 10,
+
+);
+$query2 = new WP_Query( $args2 );
+ 
+// The 2nd Loop
+while ( $query2->have_posts() ) {
+    $query2->the_post();
+    echo '<h2>' . get_the_title( $query2->post->ID ) . '</h2>';
+    echo '<p>' . SUBSTR(get_the_excerpt(),0,300) . '</p>';
+    the_post_thumbnail( 'thumbnail' );  
+}
+ 
+// Restore original Post Data
+wp_reset_postdata();
+					?>
+                </div>
+                <!-- right side -->
+                <div class="col-lg-4 event-right">
+					<?php get_sidebar(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php
+get_footer();
